e98930e6c36b8c3cf4e32ea0cddcf385f56e37a0
Danielle Bowser <37942509+queenbowsa@users.noreply.github.com>
2025-03-26 15:31:02 -0600

    chore: remove commented out code

--- app/src/main/java/com/example/fetchrewards/data/remote/network/Services.kt
+++ app/src/main/java/com/example/fetchrewards/data/remote/network/Services.kt
@@ -4,20 +4,4 @@ import com.example.fetchrewards.data.remote.dto.ItemResponse
 
 interface Services {
     suspend fun getItems(): List<ItemResponse>
-/*
-    companion object {
-        fun create(): Services {
-            return ServicesImpl(
-                client = HttpClient(OkHttp) {
-                    install(ContentNegotiation) {
-                        json(Json {
-                            ignoreUnknownKeys = true
-                            prettyPrint = false
-                            isLenient = true
-                        })
-                    }
-                }
-            )
-        }
-    }*/
 }
